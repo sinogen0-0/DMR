32ad00ea0ef236ec5ddd808a878292700e3f63ba
sinogen0-0 <59484914+sinogen0-0@users.noreply.github.com>
2026-03-28 10:29:29 -0500

    Finished Step 1 debugging

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,8 @@ const config: CapacitorConfig = {
   appName: 'Dungeon Deck Recorder',
   webDir: 'build',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'https',
+    cleartext: true
   },
   plugins: {
     SplashScreen: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$lib: `${__dirname}/src/lib`,
+			$components: `${__dirname}/src/lib/components`,
+			$services: `${__dirname}/src/lib/services`,
+			$stores: `${__dirname}/src/lib/stores`,
+			$types: `${__dirname}/src/lib/types`,
+			$utils: `${__dirname}/src/lib/utils`
+		}
+	},
 	server: {
 		port: 5173,
-		host: true,
-		middleware: []
+		host: true
 	}
 });
